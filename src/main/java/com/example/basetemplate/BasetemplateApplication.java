5d911a34bcdea64e7db59955f63db9d1d9633199
Colin Zuo <colinzuo@qq.com>
2022-02-21 14:50:33 +0800

    created using spring initializer

---
+++ src/main/java/com/example/basetemplate/BasetemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.basetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasetemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasetemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basetemplate/BasetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasetemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
